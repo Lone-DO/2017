aeacf2955fb21eb7db9dff3d5efcbdfa803957d7
lone-DO <Lone-DO@users.noreply.github.com>
2017-01-06 09:36:10 -0500

    fixed prefix

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'portfolio',
+    modulePrefix: 'lone-do.github.io',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
