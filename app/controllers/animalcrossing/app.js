8ad36b91de942d5ee6ad3ad73fedebd7eeed4200
Lone-DO <lotussythe@yahoo.com>
2017-01-11 11:29:43 -0500

    fixed Gen List and removed temporary inactive features

--- app/controllers/animalcrossing/app.js
+++ app/controllers/animalcrossing/app.js
@@ -1,53 +1,6 @@
 import Ember from 'ember';
 
 /*global $:false*/
-var load = function () {
-	var albumAPI = ('https://api.mlab.com/api/1/databases/lone-do/collections/albums?apiKey=9P6rUGDfq5OxFXag9RZYNkk3U2vF6IT0');
-	$.ajax({
-		url: albumAPI,
-		dataType: 'json',
-		success: function (data) {
-			var albums = data;
-			
-		//Api Each Loop, sets classes and displays to page
-			for (var _i = albums.length - 1; _i >= 0; _i--) {
-			//Api data storing
-				var _name = albums[_i].title,
-					_release = albums[_i].date,
-					_platform = albums[_i].platform,
-					_img = albums[_i].imageURL,
-					_hourID = albums[_i].hourID,
-					Collection = '';
-					
-				//Api loop Head/ Opening
-				Collection += '<div class="blog-post">';
-				//Api Album Model
-					Collection += '<div>';
-						Collection += '<h3>' + _name + '</h3>';
-						Collection += '<p>' + _release + '</p>';
-						Collection += '<p>' + _platform + '</p>';
-						Collection += '<img src="' + _img + '"">';
-					Collection += '</div>';
-					Collection += '<div>';
-						Collection += '<ul>';
-
-						for (var h = _hourID.length - 1; h >= 0; h--) {
-							var _hour = _hourID[h],
-								list = '<li>' + _hour + '</li>';
-							Collection += list;
-						}
-
-						Collection += '</ul>';
-					Collection += '</div>';
-				//Api loop closing
-					Collection += '</div>';
-				//Posting Api data
-				$('#gens').append(Collection);
-			}
-		}
-	});
-};
-
 export default Ember.Controller.extend({
 	actions: {
 		isStarted: false, //defaults app to not started
@@ -55,6 +8,7 @@ export default Ember.Controller.extend({
 		hintShowing: false, //defaults guide to not show
 	//Onclick of Launch img, Fire application
 		end: function () {
+            this.set('genShowing', false);
             this.set('isStarted', false);
         },
 		start: function () {
@@ -64,8 +18,7 @@ export default Ember.Controller.extend({
 			$(function phases() {
 				var lastHr = -1,
 					lastMin = -1,
-					lastSec = -1,
-					posted = false;
+					lastSec = -1;
 				
 			//*** Setting up API ***
 				$(function () {
@@ -164,13 +117,6 @@ export default Ember.Controller.extend({
 								if (hours < 10) {hours = "0" + hours; }
 								if (minutes < 10) {minutes = "0" + minutes; }
 								if (seconds < 10) {seconds = "0" + seconds; }
-								if (!posted && currentUrl.indexOf("/gen") > -1 ) {
-									load();
-									posted = true;
-								}
-								$('.cancel-button').click(function (){
-									posted = false;
-								});
 
 								var play = function () {
 									if (hours < 10) {
