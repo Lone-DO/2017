c3e7552f1e14fa2e685cc937be17e37d717118dc
lone-DO <Lone-DO@users.noreply.github.com>
2017-07-21 10:54:02 -0400

    Successfully Pushes API data to Mongodb on button interactions

--- app/controllers/sinmod.js
+++ app/controllers/sinmod.js
@@ -1,17 +1,16 @@
 ﻿import Ember from 'ember';
 
+let ballet = "",
+    $stringed = "",
+    balletAPI = "https://api.mlab.com/api/1/databases/lone-do/collections/ballets?apiKey=9P6rUGDfq5OxFXag9RZYNkk3U2vF6IT0",
+    serverAPI = "https://7daystodie-servers.com/api/?object=servers&element=voters&key=ie9uc889pv9tq8czt1h44vckjnfq81vp0i&month=current&format=json";
+
 export default Ember.Controller.extend({
     actions: {
         serverLog: function () {
             $(function phases() {
                 // Authenticate via API Key
-                let serverAPI = ('https://7daystodie-servers.com/api/?object=servers&element=voters&key=ie9uc889pv9tq8czt1h44vckjnfq81vp0i&month=current&format=json'),
-                    _name = '',
-                    _address = '',
-                    _port = '',
-                    _month = '',
-                    _voters = '',
-                    _nickname = '',
+                let _nickname = '',
                     _votes = '',
                     log = '';
                 console.log('Api location is ' + serverAPI);
@@ -23,17 +22,18 @@ export default Ember.Controller.extend({
                         const info = data.voters;
                         let date = new Date(),
                             locale = "en-us",
-                            month = date.toLocaleString(locale, { month: "short" }),
-                            monthName = date.toLocaleString(locale, { month: "long" }),
-                            n = $('.sinData').length,
-                            ballet = { Jul: { "voters" : info } };
-                        console.log(ballet);
+                            month = date.toLocaleString(locale, { month: "short" }), //Dec
+                            monthName = date.toLocaleString(locale, { month: "long" }), //December
+                            n = $('.sinData').length;
+
+                        ballet = { Jul: { "voters": info } };// Compiles data
+                        $stringed = JSON.stringify(ballet); // serealizes Array into json string
                        
-                        if (n > 0) { //Wipes the div if already loaded
-                            $('.sinData').html(`<h3>${monthName}</h3> <span>${ballet}</span>`);
+                        if ($('.sinData').length > 0) { //Wipes the div if already loaded
+                            $('.sinData').html(`<h3>${monthName}</h3>`);
                         }
 
-                        for (let i = 0; i < info.length; i++) {
+                        for (let i = 0; i < info.length; i++) { //loops through array & inputs
                             _nickname = info[i].nickname,
                                 _votes = info[i].votes;
 
@@ -49,6 +49,25 @@ export default Ember.Controller.extend({
                     }
                 });
             });
+        },
+        serverUpload: function () {
+            $.ajax({
+                'type': 'POST',
+                'url': balletAPI,
+                'contentType': 'application/json',
+                'data': JSON.stringify(ballet),
+                'dataType': 'json',
+                'success': function (save) {
+                    alert(`Upload was a Success!, Here's what was uploaded... ${$stringed}`);
+                },
+                error: function () {
+                    alert("Error Saving Data... Check that you have Pulled the data first before uploading");
+                }
+            });
         }
     }
 });
+
+//setInterval(function () {
+//    console.error($stringed);
+//}, 10000);
