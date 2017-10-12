9e17878a15048e18bdfefdcddda800016a771598
lone-DO <Lone-DO@users.noreply.github.com>
2017-10-12 18:23:20 -0400

    Cancelling plugin insertion!
    Plugin clashes too much with all of Bootstrap 4's modules and tools,
    need to scrap "Swiper" but keep minor edits.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,12 +3,29 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 });
 
-
+//initialize swiper when document ready
 $(document).ready(function () {
   console.log("plugin loaded");
   //Initialize Swiper
+  //var swiper = new Swiper('.swiper-container', {
+  //  slidesPerView: 1,
+  //  spaceBetween: 50,
+  //  slidesPerGroup: 1,
+  //  loop: false,
+  //  loopFillGroupWithBlank: false,
+  //  //pagination: {
+  //  //  el: '.swiper-pagination',
+  //  //  clickable: true,
+  //  //},
+  //  navigation: {
+  //    nextEl: '.swiper-button-next',
+  //    prevEl: '.swiper-button-prev',
+  //  },
+  //});
   var swiperH = new Swiper('.swiper-container-h', {
+    direction: 'horizontal',
     spaceBetween: 50,
+    //loop: true,
     pagination: {
       el: '.swiper-pagination-h',
       clickable: true,
@@ -17,17 +34,10 @@ $(document).ready(function () {
   var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
     spaceBetween: 50,
+    ////loop: true,
     pagination: {
       el: '.swiper-pagination-v',
       clickable: true,
-    },
+    }
   });
-
-
-  ////initialize swiper when document ready
-  //var mySwiper = new Swiper('.swiper-container', {
-  //  // Optional parameters
-  //  direction: 'horizontal',
-  //  loop: true
-  //})
 });
